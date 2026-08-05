bec279ff419aa3661072999a98f8cad79bda06bc
GarryChen0402 <gychen0402@foxmail.com>
2026-08-05 10:38:32 +0800

    15

---
+++ Codes/15.cpp
@@ -0,0 +1,30 @@
+#include "../header.h"
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+
+        vector<vector<int>> ans;
+        int n = nums.size();
+        for(int i=0;i<n-2;i++){
+            if(i > 0 && nums[i] == nums[i-1])continue;
+            int l = i+1;
+            int r = n-1;
+            while(l < r){
+                int tmp = nums[i] + nums[l] + nums[r];
+                if(tmp > 0){
+                    r--;
+                }else if(tmp < 0){
+                    l++;
+                }else{
+                    ans.push_back({nums[i], nums[l], nums[r]});
+                    r--;
+                    while(r > l && nums[r] == nums[r+1])r--;
+                    l++;
+                    while(l < r && nums[l] == nums[l-1])l++;
+                }
+            }
+        }
+        return ans;
+    }
+};
